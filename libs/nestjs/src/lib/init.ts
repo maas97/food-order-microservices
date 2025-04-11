08e65baa61ecec76642fa901a91b5df29d35d084
maas97 <mashraf.14697@gmail.com>
2025-04-11 18:42:24 +0200

    feat: add nestjs library

---
+++ libs/nestjs/src/lib/init.ts
@@ -0,0 +1,19 @@
+import { ConfigService } from '@nestjs/config';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { Logger } from 'nestjs-pino';
+
+export async function init(app: INestApplication) {
+  const globalPrefix = 'api';
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix(globalPrefix);
+  app.useLogger(app.get(Logger));
+  app.use(cookieParser());
+  const port = app.get(ConfigService).getOrThrow('PORT');
+  await app.listen(port);
+  app
+    .get(Logger)
+    .log(
+      `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    );
+}
